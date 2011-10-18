2e9d7191f2f5a31876af651010be30376aeffbb3
Pavel Karasov <pavel@karasov.net>
2011-10-18 09:46:24 +0400

    Clear repositore

--- ant/tools/test_bot.cmd
+++ ant/tools/test_bot.cmd
@@ -1,2 +1,3 @@
 @echo off
-playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map %1 "python submission_test/TestBot.py" -e --nolaunch --strict --capture_errors
+copy /y ..\bin\Debug\ant.exe ant.exe
+playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map ant.exe "python submission_test/TestBot.py" -e --nolaunch --strict --capture_errors
