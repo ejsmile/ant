529b506e838c578e667db00afdc0c06f69576ed9
Pavel Karasov <pavel@karasov.net>
2011-10-18 22:42:40 +0400

    Add start script to play my bot on big maps and tutorial maps

--- ant/tools/play_one_game.sh
+++ ant/tools/play_one_game.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/multi_hill_maze/multi_maze_07.map "$@" "python sample_bots/python/HunterBot.py" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
+rm -f log.log
+rm -f debug.log
+
+./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/multi_hill_maze/multi_maze_07.map "$@" "ant.exe" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
--- ant/tools/test_bot.sh
+++ ant/tools/test_bot.sh
@@ -1,1 +1,4 @@
-./playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map "$1" "python submission_test/TestBot.py" --nolaunch -e --strict --capture_errors
+rm -f log.log
+rm -f debug.log
+cp -f ../bin/Debug/ant.exe ant.exe
+./playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --map_file submission_test/test.map "ant.exe" "python submission_test/TestBot.py" --nolaunch -e --strict --capture_errors
