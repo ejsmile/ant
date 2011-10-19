25669df48bfb349d0079978fa0ba9ecce31a6f0a
Pavel Karasov <pavel@karasov.net>
2011-10-19 17:42:49 +0400

    Simple algoritm mass hunt/food

--- ant/MyBot.cs
+++ ant/MyBot.cs
@@ -10,7 +10,7 @@ namespace Ants
 		private Random rng;
 		private HashSet<Location> destinations;
 		private IDictionary<AntLoc, Location> currentTurn;
-		private IDictionary<AntLoc, Location> oldTurn;
+		//private IDictionary<AntLoc, Location> oldTurn;
 		private FileStream fs;
 		private StreamWriter sw;
 		private int number = 1;
@@ -20,7 +20,7 @@ namespace Ants
 			rng = new Random (42);
 			destinations = new HashSet<Location> (new LocationComparer ());
 			currentTurn = new Dictionary<AntLoc, Location> ();
-			oldTurn = new Dictionary<AntLoc, Location> ();
+			//oldTurn = new Dictionary<AntLoc, Location> ();
 			fs = new FileStream ("log.log", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write);
 			sw = new StreamWriter (fs);
 		}
@@ -31,53 +31,59 @@ namespace Ants
 			sw.WriteLine ("!Turn " + number++);
 			destinations.Clear ();
 			currentTurn.Clear ();
-
+			state.EnemyHills.AddRange(state.EnemyHills);
+			
 			foreach (var ant in state.MyAnts) {
-				if (oldTurn.ContainsKey (ant)) {
-					if (state.FoodTiles.Contains (oldTurn [ant])) { 
-						sw.WriteLine (ant + " old food aim" + oldTurn [ant]);
-						//state.EnemyAnts.Contains (new AntLoc (oldTurn [ant], 0))) {
-						currentTurn.Add (ant, oldTurn [ant]);
-						//Цель есть она не достигнута и видна
-						continue;
-					}
-					sw.WriteLine(!ant.Equals( oldTurn [ant]) + " " + ((ant.row != oldTurn [ant].row) && (ant.col != oldTurn [ant].col)) );
-					//жратвы нет и но рандомная цель не достигнута
-					if ((state.FoodTiles.Count == 0)
-						//&& (ant != oldTurn [ant])) {
-						&& ((ant.row != oldTurn [ant].row) && (ant.col != oldTurn [ant].col))) {
-						sw.WriteLine (ant + " old random aim" + oldTurn [ant]);
-						currentTurn.Add (ant, oldTurn [ant]);
-						continue;
-					}
-				}
-
 				//TODO find new Aim
-
-				int dist = int.MaxValue;
-				Location aim = null;
+				
+				int distEnemy = int.MaxValue;
+				Location aimEnemy = null;
 					
-				foreach (var food in state.FoodTiles) {
-					if (state.distance (ant, food) < dist) {
-						dist = state.distance (ant, food);
-						aim = food;
+				foreach (var enemy in state.EnemyAnts) {
+					if (state.distance (ant, enemy) < distEnemy) {
+						distEnemy = state.distance (ant, enemy);
+						aimEnemy = enemy;
 					}			
 				}
-				if (aim != null) {
-					sw.WriteLine (ant + " goto new food " + aim);
-					currentTurn.Add (ant, aim);
-				} else {
+				
+				
+				int distFood = int.MaxValue;
+				Location aimFood = null;
+					
+				foreach (var food in state.FoodTiles) {
+					if (state.distance (ant, food) < distFood) {
+						distFood = state.distance (ant, food);
+						aimFood = food;
+					}			
+				}
+				
+				if ((aimFood == null) && (aimEnemy == null)) {
 					int shaftRow = (state.ViewRadius2 + rng.Next (state.ViewRadius2 * 2)) * ((-1) ^ rng.Next (2));
 					int shaftCol = (state.ViewRadius2 + rng.Next (state.ViewRadius2 * 2)) * ((-1) ^ rng.Next (2));
 					
-					aim = state.destination (ant, new Location (ant.row + shaftRow, ant.col + shaftCol));
-					sw.WriteLine (ant + " goto new random " + aim);
-					currentTurn.Add (ant, aim);
+					aimFood = state.destination (ant, new Location (ant.row + shaftRow, ant.col + shaftCol));
+					sw.WriteLine (ant + " goto new random " + aimFood);
+					currentTurn.Add (ant, aimFood);
+					continue;
 				}
+				
+				if ((aimFood != null) && (aimEnemy != null)) {
+					if (distEnemy < distFood)
+						currentTurn.Add (ant, aimEnemy);
+					else
+						currentTurn.Add (ant, aimFood);
+					continue;
+				}
+				if (aimFood == null) {
+					currentTurn.Add (ant, aimEnemy);
+					continue;
+				} else {
+					currentTurn.Add (ant, aimFood);
+				}
+				
 			}
 			
 			//Clear oldTurn for new data
-			oldTurn = new Dictionary<AntLoc, Location> ();
 			//move ant to thei aim
 			foreach (AntLoc ant in currentTurn.Keys) {
 			
@@ -100,10 +106,7 @@ namespace Ants
 					
 						if (!destinations.Contains (NewAnt)) {
 							destinations.Add (NewAnt);
-							oldTurn.Add (NewAnt, currentTurn [ant]);
 							issueOrder (ant, direction);
-						} else {
-							oldTurn.Add (ant, currentTurn [ant]);
 						}
 						break;
 					}
