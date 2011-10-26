4dff97b6d6fa00b82ef3ec4527c0c1c6fbfc339e
Pavel Karasov <pavel@karasov.net>
2011-10-26 23:54:46 +0400

    fix test

--- ant/tools/mytest.sh
+++ ant/tools/mytest.sh
@@ -2,4 +2,4 @@ rm -f log.log
 rm -f debug.log
 cp -f ../bin/Debug/ant.exe ant.exe
 
-./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/example/tutorial1.map "$@" "ant.exe"  "python sample_bots/python/GreedyBot.py"
+./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 200 --map_file maps/example/tutorial1.map "$@" "ant.exe"  "python sample_bots/python/GreedyBot.py"
